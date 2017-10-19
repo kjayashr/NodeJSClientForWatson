2d7e5e381811cfaa54cece9947f1ff02f61093db
kjayashr <kkanthjc@gmail.com>
2017-10-19 02:03:43 -0700

    Paging and links updated

--- static_web/login.php
+++ static_web/login.php
@@ -94,15 +94,18 @@
     <?php
     if (isset($_POST['submit']))
         {
-    include("config.php");
+				echo "<script>console.log( );</script>";
+    include("/nodeclient/ config.php");
     session_start();
     $username=$_POST['username'];
     $password=$_POST['password'];
+	echo "<script>console.log( 'Username: " . $username . "' );</script>";
+	echo "<script>console.log( 'Password: " . $password . "' );</script>";
     $_SESSION['login_user']=$username;
     $query = mysql_query("SELECT username FROM login WHERE username='$username' and password='$password'");
      if (mysql_num_rows($query) != 0)
     {
-     echo "<script language='javascript' type='text/javascript'> location.href='home.php' </script>";
+     echo "<script language='javascript' type='text/javascript'> location.href='/nodeclient/home.php' </script>";
       }
       else
       {
